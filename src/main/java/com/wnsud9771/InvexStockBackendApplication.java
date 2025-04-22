4065938e713c28b2453c6d4278c736e3f47a90cf
jun9771 <jun9771@gmail.com>
2025-04-22 23:39:15 +0900

    firstc

---
+++ src/main/java/com/wnsud9771/InvexStockBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wnsud9771;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvexStockBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvexStockBackendApplication.class, args);
+	}
+
+}
